c27ae6f1a3506e66dd36d878053aec60dceb3d62
Abir <abirovi481@gmail.com>
2023-03-03 00:07:02 +0600

    sixth commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const displayData = (universes, dataLimit) =>{
         cardDiv.classList.add("col")
         cardDiv.innerHTML = `
         <div class="card h-100 shadow-lg rounded-2">
-            <img src="${universe.image}" class="card-img-top" alt="...">
+            <img src="${universe.image}" class="card-img-top" alt="">
             <div class="card-body p-3">
                 <h5 class="card-title">Features</h5>
                 <p class="card-text">
@@ -46,7 +46,7 @@ const displayData = (universes, dataLimit) =>{
                     <h5 class="card-title">${universe.name}</h5>
                     <p class="card-text"><i class="fa-solid fa-calendar-days mx-1"></i><small>${universe.published_in}</small></p>
                </div>
-               <button class="btn btn-outline-dark w-25 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></button>
+               <button onclick = "loadDetails('${universe.id}')" class="btn btn-outline-dark w-25 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></button>
             </div>
         </div>
         `
@@ -72,16 +72,67 @@ const toggleSpinner = isLoading => {
 }
 // load details 
 const loadDetails = id =>{
-    const url = `https://openapi.programming-hero.com/api/ai/tools/${id}`
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displayDetails(data.data.tools))
+    .then(data => displayDetails(data.data))
 }
 // displayDetails 
-const displayDetails = () => {
-    
+const displayDetails = universe => {
+    const universeDetails = document.getElementById('universe-details')
+    universeDetails.innerHTML=`
+    <div class ="d-flex justify-content-between gap-4">
+        <div class="border border-danger rounded p-3">
+           <h5>${universe.description}<h4>
+           <div class ="d-flex justify-content-between mt-3">
+              <p style="font-size: 16px;" class="text-success">
+                <small>
+                    <span>${universe.pricing[0].price ? universe.pricing[0].price: "Free of Cost"}</span>
+                    <br>
+                    <span>${universe.pricing[0].plan}</span>
+                </small>
+              </p>
+              <p style="font-size: 16px;" class="text-warning">
+                <small>
+                    <span>${universe.pricing[1].price ? universe.pricing[1].price: "Free of Cost"}</span>
+                    <br>
+                    <span>${universe.pricing[1].plan}</span>
+                </small>
+              </p>
+              <p style="font-size: 16px;" class="text-danger">
+                <small>
+                    <span>${universe.pricing[2].price ? universe.pricing[2].price: "Free of Cost"}</span>
+                    <br>
+                    <span>${universe.pricing[2].plan}</span>
+                </small>
+              </p>
+           </div>
+           <div class ="d-flex justify-content-between mt-3">
+               <div>
+                  <h5>Features</h5>
+                    <ul>
+                        <li><small>${universe.features}</small></li>
+                        <li><small>${universe.features}</small></li>
+                        <li><small>${universe.features}</small></li>
+                    </ul>
+               </div>
+               <div>
+                   <h5>Integrations</h5>
+                    <ul>
+                        <li><small>${universe.integrations[0] ? universe.integrations[0]: "No data Found"}</small></li>
+                        <li><small>${universe.integrations[1] ? universe.integrations[1]: "No data Found"}</small></li>
+                        <li><small>${universe.integrations[2] ? universe.integrations[2]: "No data Found"}</small></li>
+                    </ul>
+               </div>
+           </div>
+        </div>
+        <div>
+           <img src="${universe.image_link[0]}" class="card-img-top rounded" alt="">
+           <h5>${universe.input_output_examples}</h5>
+        </div>
+    </div> 
+    `
 }
-
 
 
 loadData(6, toggleSpinner(true))
